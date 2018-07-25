657cca6be53f569cafbe9a3e56591e080bc84758
Ryan <ryan.elr@outlook.com>
2018-07-25 14:25:41 +0200

    ex1

---
+++ index.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice1 Part1</title>
+  </head>
+  <body>
+    <?php
+    $name = 'Ryan';
+    echo $name;
+    ?>
+  </body>
+</html>
